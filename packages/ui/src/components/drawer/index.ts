5ee210fc42b765577807d7475d4b46d7ce8eb3ff
Johnny <johnnyflores@live.ca>
2026-08-23 19:57:23 -0400

    refactor(ui): add component barrel exports

---
+++ packages/ui/src/components/drawer/index.ts
@@ -0,0 +1,14 @@
+export { Drawer } from "./Drawer";
+export type { DrawerProps } from "./Drawer";
+
+export { DrawerHeader } from "./DrawerHeader";
+export type { DrawerHeaderProps } from "./DrawerHeader";
+
+export { DrawerTitle } from "./DrawerTitle";
+export type { DrawerTitleProps } from "./DrawerTitle";
+
+export { DrawerContent } from "./DrawerContent";
+export type { DrawerContentProps } from "./DrawerContent";
+
+export { DrawerFooter } from "./DrawerFooter";
+export type { DrawerFooterProps } from "./DrawerFooter";
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,29 +1,6 @@
-export { Button } from "./components/button/Button";
-export type { ButtonProps } from "./components/button/Button";
-
-export { Drawer } from "./components/drawer/Drawer";
-export type { DrawerProps } from "./components/drawer/Drawer";
-
-export { DrawerHeader } from "./components/drawer/DrawerHeader";
-export type { DrawerHeaderProps } from "./components/drawer/DrawerHeader";
-
-export { DrawerTitle } from "./components/drawer/DrawerTitle";
-export type { DrawerTitleProps } from "./components/drawer/DrawerTitle";
-
-export { DrawerContent } from "./components/drawer/DrawerContent";
-export type { DrawerContentProps } from "./components/drawer/DrawerContent";
-
-export { DrawerFooter } from "./components/drawer/DrawerFooter";
-export type { DrawerFooterProps } from "./components/drawer/DrawerFooter";
-
-export { Badge } from "./components/badge/Badge";
-export type { BadgeProps } from "./components/badge/Badge";
-
-export { ThemeProvider, useTheme } from "./theme";
-export type { Theme } from "./theme";
-
-export { Typography } from "./components/typography/Typography";
-export type { TypographyProps } from "./components/typography/Typography";
-
-export { Header } from "./components/header/Header";
-export type { HeaderProps } from "./components/header/Header";
+export * from "./components/badge";
+export * from "./components/button";
+export * from "./components/drawer";
+export * from "./components/header";
+export * from "./components/typography";
+export * from "./theme";
