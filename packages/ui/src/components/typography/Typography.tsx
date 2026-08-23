9ce9401d0218e3ef91fb4457f13502b835b697ce
Johnny <johnnyflores@live.ca>
2026-08-23 18:00:34 -0400

    feat(ui): add Typography variants

--- packages/ui/src/components/typography/Typography.tsx
+++ packages/ui/src/components/typography/Typography.tsx
@@ -3,19 +3,24 @@ import "./Typography.scss";
 
 type TypographyElement = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
 
+export type TypographyVariant = "display" | "body" | "body-sm" | "caption";
+
 export type TypographyProps = React.HTMLAttributes<HTMLElement> & {
   as?: TypographyElement;
+  variant?: TypographyVariant;
 };
 
 export function Typography({
   as: Component = "p",
-  children = "",
+  variant,
+  children,
   className = "",
   ...props
 }: TypographyProps) {
+  const variantClass = variant ? `ui-typography--variant-${variant}` : "";
   return (
     <Component
-      className={`ui-typography ui-typography--${Component} ${className}`}
+      className={`ui-typography ui-typography--${Component} ${variantClass} ${className}`}
       {...props}
     >
       {children}
